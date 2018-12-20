7d4931a6105621754b85fecb355afcbb6716ee68
Gigantic Tree <seqs@dartmouth.edu>
2018-12-20 15:30:14 -0500

    add hover over effect, button to choose new grid size. grid size is not yet dynamic

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const square = document.querySelector('.gridPiece');
+let gridsize = '16';
 container = document.querySelector('.container');
-for (i=0;i<16*16;i++) {
-    let gridPiece = document.createElement('div');
-    gridPiece.classList.add('gridPiece');
-    container.appendChild(gridPiece);
+drawGrid(gridsize);
+function drawGrid(gridsize) {
+    for (i=0;i<gridsize*gridsize;i++) {
+        let gridPiece = document.createElement('div');
+        gridPiece.classList.add('gridPiece');
+        gridPiece.classList.add(i);
+        container.appendChild(gridPiece);
+    }
 }
+
+window.addEventListener('mouseover', function(square) {
+    square.classList.add('touched');
+});
+
+let button = document.querySelector('button');
+button.addEventListener('click',function() {
+    gridsize = prompt('How many tiles wide would you like the grid?');
+    drawGrid(gridsize);
+})
