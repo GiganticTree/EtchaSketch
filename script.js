8f148ef5f0b6b9bed457af49858cefa09039300a
Gigantic Tree <seqs@dartmouth.edu>
2018-12-20 14:37:23 -0500

    added 16x16 grid using css and javascipt

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 container = document.querySelector('.container');
-for (i=0;i<16;i++) {
-
+for (i=0;i<16*16;i++) {
+    let gridPiece = document.createElement('div');
+    gridPiece.classList.add('gridPiece');
+    container.appendChild(gridPiece);
 }
